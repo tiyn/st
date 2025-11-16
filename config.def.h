2ac4600c555e56e1479d186786d879168434c207
tiyn <tiyn@posteo.eu>
2025-11-16 09:28:03 +0100

    patched font2

--- config.def.h
+++ config.def.h
@@ -8,6 +8,12 @@
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
 static int borderpx = 2;
 
+/* Spare fonts */
+static char *font2[] = {
+/*	"Inconsolata for Powerline:pixelsize=12:antialias=true:autohint=true", */
+/*	"Hack Nerd Font Mono:pixelsize=11:antialias=true:autohint=true", */
+};
+
 /*
  * What program is execed by st depends of these precedence rules:
  * 1: program passed with -e
